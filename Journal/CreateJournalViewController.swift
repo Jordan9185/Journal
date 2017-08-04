0e987efee6cfbe5de81bfdd282d33efd92fc258a
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-08-04 10:27:26 +0800

    Fine tune UI for Create page.

--- Journal/CreateJournalViewController.swift
+++ Journal/CreateJournalViewController.swift
@@ -10,26 +10,28 @@ import UIKit
 
 class CreateJournalViewController: UIViewController {
 
+    @IBOutlet var loaderImageView: UIImageView!
+    
+    let journalManager = JournalManager()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setloaderImageViewConfig()
+        
+        
     }
 
+    func setloaderImageViewConfig() {
+        
+        loaderImageView.tintColor = .white
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
