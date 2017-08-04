0b0322d0c961f01d1bc8777434302458c328695f
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-08-04 10:50:02 +0800

    Implement basic function for CreateNewPage.

--- Journal/CreateJournalViewController.swift
+++ Journal/CreateJournalViewController.swift
@@ -8,9 +8,19 @@
 
 import UIKit
 
+enum CreateJournalError:Error {
+    
+    case ImageIsNil,ImageConvertFail
+    
+}
+
 class CreateJournalViewController: UIViewController {
 
     @IBOutlet var loaderImageView: UIImageView!
+    
+    @IBOutlet var titleTextField: UITextField!
+    
+    @IBOutlet var contentTextField: UITextField!
     
     let journalManager = JournalManager()
     
@@ -27,11 +37,64 @@ class CreateJournalViewController: UIViewController {
         
         loaderImageView.tintColor = .white
         
+        loaderImageView.isUserInteractionEnabled = true
+        
+        loaderImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(pickImageAction)))
+        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func pickImageAction() {
+        
+        print("pickImageAction()")
+        
+    }
+    
+    @IBAction func saveJournalActionTapped(_ sender: UIButton) throws {
+        
+        let title = titleTextField.text ?? ""
+        
+        let content = contentTextField.text ?? ""
+        
+        guard let image = loaderImageView.image else {
+            
+            throw CreateJournalError.ImageIsNil
+            
+        }
+        
+        guard let imageData = UIImagePNGRepresentation(image) else {
+            
+            throw CreateJournalError.ImageConvertFail
+            
+        }
+        
+        if title == "" {
+            
+            showWarningforTitleIsEmpty()
+            
+        }
+        
+        journalManager.addJournal(title: title, imageData: imageData, content: content)
+        
+        
+    
+    }
+    
+    func showWarningforTitleIsEmpty() {
+        
+        let alertController = UIAlertController(title: "Empty", message: "Title must exist.", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cancelCreateEvent(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
     }
 
 }
